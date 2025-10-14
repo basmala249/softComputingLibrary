7b9977a234c3a41632619480b0b4e0176e6633ad
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-10-14 18:56:37 +0300

    3rd commit

--- Chromosomes/PermutationChromosome.java
+++ Chromosomes/PermutationChromosome.java
@@ -1,5 +1,13 @@
 package Chromosomes;
 
-public class PermutationChromosome {
-    
+public class PermutationChromosome  implements Chromosome{
+    @Override
+    public void InitializeChromosome() {
+
+    }
+
+    @Override
+    public void PrintChromosome() {
+
+    }
 }
