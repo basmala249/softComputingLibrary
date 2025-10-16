38126dc60bbd19324a4168b4b8949c74d8f801c0
mayafouad <11410120220270@stud.cu.edu.eg>
2025-10-16 11:03:23 +0300

    Implement Fitness Function

--- FitnessFunctions/N_QueensCaseStudyFitnessFunction.java
+++ FitnessFunctions/N_QueensCaseStudyFitnessFunction.java
@@ -2,17 +2,27 @@ package FitnessFunctions;
 import Chromosomes.Chromosome;
 
 public class N_QueensCaseStudyFitnessFunction implements IFitnessFunction<Chromosome<Integer>> {
-    private final int n;
+    private final int N;
 
-    public N_QueensCaseStudyFitnessFunction(int n) {
-        this.n = n;
+    public N_QueensCaseStudyFitnessFunction(int N) {
+        this.N = N;
     }
 
     @Override
     public double evaluate(Chromosome<Integer> chromosome) {
         double fitness = 0.0 ;
         //Implement Fitness Function
-
+        int conflicts = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                // queens attack each other if they share a diagonal
+                if (Math.abs(chromosome.getIndex(i) - chromosome.getIndex(j)) == Math.abs(i - j)) {
+                    conflicts++;
+                }
+            }
+        }
+        int maxPairs = N * (N - 1) / 2;
+        fitness = maxPairs - conflicts;
         return fitness;
     }
     
