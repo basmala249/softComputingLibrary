4466e3074e9c0090a1e11ba6b3b2f74b2c558c61
Ahmed Ashraf <lamoro8800@gmail.com>
2025-10-18 22:35:23 +0300

    Feat: implement FloatChromosome and FloatMutationStrategy classes

--- MutationStratgey/FloatMutationStrategy.java
+++ MutationStratgey/FloatMutationStrategy.java
@@ -2,8 +2,23 @@ package MutationStratgey;
 
 import Chromosomes.Chromosome;
 
+import java.util.Random;
 
-public interface FloatMutationStrategy<T> extends IMutation<T> {
+
+public interface FloatMutationStrategy<T extends Number> extends IMutation<T> {
     @Override
-    public Chromosome<T> mutate(Chromosome<T> chromosome) ;
+    public default Chromosome<T> mutate(Chromosome<T> chromosome) {
+        int length = chromosome.getSize();
+        Random random = new Random();
+        double L = 0, R = 100; /// the gene range is [0.0, 100.0] you can change it if you want
+        for(int i = 0; i < length; ++i) {
+            double val = chromosome.getIndex(i).doubleValue();
+            double r = random.nextDouble();
+            double mx = r <= 0.5 ? val - L : R - val;
+            double r1 = random.nextDouble() * mx;
+            double newVal = r <= 0.5 ? val - r1 : val + r1;
+            chromosome.setIndex(i, (T) Double.valueOf(newVal));
+        }
+        return chromosome;
+    }
 }
