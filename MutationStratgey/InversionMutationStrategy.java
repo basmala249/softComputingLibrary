9cb29ac5064bde675b72c9da8c06bfd99a61e2f2
Ahmed Ashraf <lamoro8800@gmail.com>
2025-10-21 00:40:32 +0300

    Fix typo in condition and use a local array in InversionMutation instead of modifying the main array. Correct logic in RouletteWheelSelection for the isMinimization case and improve related optimizations.

--- MutationStratgey/InversionMutationStrategy.java
+++ MutationStratgey/InversionMutationStrategy.java
@@ -6,25 +6,27 @@ public class InversionMutationStrategy <T> implements IMutation<T> {
 
     @Override
     public Chromosome<T> mutate(Chromosome<T> chromosome) {
-        int length = chromosome.getSize();
+        Chromosome<T> mutated = chromosome.copy();
+        int length = mutated.getSize();
         int index1 = (int) (Math.random() * length);
         int index2 = (int) (Math.random() * length);
         while (index2 == index1){
             index2 = (int) (Math.random() * length);
         }
-        if(index2 > index1){
+        /// index1 always should be less than index2
+        if(index2 < index1){
             int t = index1;
             index1 = index2;
             index2 = t;
         }
         while (index1<index2){
-            T temp = chromosome.getIndex(index1);
-            chromosome.setIndex(index1,chromosome.getIndex(index2));
-            chromosome.setIndex(index2,temp);
+            T temp = mutated.getIndex(index1);
+            mutated.setIndex(index1,mutated.getIndex(index2));
+            mutated.setIndex(index2,temp);
             index1++;
             index2--;
         }
 
-        return chromosome;
+        return mutated;
     }
 }
--- SelectionStratgey/RankSelection.java
+++ SelectionStratgey/RankSelection.java
@@ -19,7 +19,7 @@ public class RankSelection<T> implements SelectionInterface<T> {
         int n = copyOfChromosomes.size();
         // Sort chromosomes based on fitness
         IFitnessFunction<T> fitnessFunction = copyOfChromosomes.get(0).getFitnessFunction();
-        copyOfChromosomes.sort((ch1, ch2) -> Double.compare(fitnessFunction.evaluate(ch1), fitnessFunction.evaluate(ch2)));
+        copyOfChromosomes.sort((ch1, ch2) -> Double.compare(fitnessFunction.evaluate(ch2), fitnessFunction.evaluate(ch1)));
 
         // If the problem is minimization, reverse the sorted list
         // Bec we want the worst to have the lowest rank
@@ -32,11 +32,26 @@ public class RankSelection<T> implements SelectionInterface<T> {
         List<Integer> ranks = getRanks(copyOfChromosomes);
         List<Chromosome<T>> selectedChromosomes = new ArrayList<>();
 
-        int upper_limit = (n * (n + 1) / 2) + 1;
+        int upper_limit = (n * (n + 1) / 2);
 
         // Select chromosomes based on ranks
         for(int i = 0; i < numberToBeSelected;i++) {
+<<<<<<< HEAD
             int randomNum = random.nextInt(upper_limit- 1) + 1;
+=======
+            int randomNum = random.nextInt(upper_limit) + 1;
+            System.out.println("Debug>> Random Number Generated for Rank Selection :: " + randomNum);
+            int targetChromosomeIndex = SearchHelpers.lowerBound(ranks, randomNum);
+            System.out.print("Debug>> Target Chromosome Index Selected for Rank Selection :: " + targetChromosomeIndex);
+            copyOfChromosomes.get(targetChromosomeIndex).PrintChromosome();
+            selectedChromosomes.add(copyOfChromosomes.get(targetChromosomeIndex));
+
+
+        }
+           for(int i = 0;i < selectedChromosomes.size();i++) {
+            System.out.print("Debug>> Sorted Chromosomes for Rank Selection :: " + " Fitness:: " + fitnessFunction.evaluate(selectedChromosomes.get(i)));
+            selectedChromosomes.get(i).PrintChromosome();
+>>>>>>> 7dc59c6 (Fix typo in condition and use a local array in InversionMutation instead of modifying the main array.)
             
             int targetChromosomeIndex = SearchHelpers.lowerBound(ranks, randomNum);
             selectedChromosomes.add(copyOfChromosomes.get(targetChromosomeIndex));
