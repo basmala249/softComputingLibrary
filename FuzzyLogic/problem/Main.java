948def58531c1f8198702c821b3b25f8b0e1d39c
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-11-08 20:20:47 +0200

    15th commit

--- FuzzyLogic/problem/Main.java
+++ FuzzyLogic/problem/Main.java
@@ -15,39 +15,39 @@ import MemberFunction.TrapzoidFunction;
 public class Main {
     public static Integer INF = 1000000;
     public static void main(String[] args) {
-        String Variable = "Temperature" , fuzzySet1 = "Cold" , fuzzySet2 = "Warm" , fuzzySet3 = "Hot";
-        List<String> FuzzysetNames = List.of(fuzzySet1, fuzzySet2, fuzzySet3);
-        FuzzySet<Double> fs = new FuzzySet<>();
-        List<Double> inputs = List.of(0.0, 0.0, 20.0, 40.0);
-        IMemberFunction<Double> mf1 = new TrapzoidFunction<Double>(inputs);
-        List<Double> yValues = new ArrayList<>();
+        // String Variable = "Temperature" , fuzzySet1 = "Cold" , fuzzySet2 = "Warm" , fuzzySet3 = "Hot";
+        // List<String> FuzzysetNames = List.of(fuzzySet1, fuzzySet2, fuzzySet3);
+        // FuzzySet<Double> fs = new FuzzySet<>();
+        // List<Double> inputs = List.of(0.0, 0.0, 20.0, 40.0);
+        // IMemberFunction<Double> mf1 = new TrapzoidFunction<Double>(inputs);
+        // List<Double> yValues = new ArrayList<>();
 
-        yValues = getY(mf1.getPoints());
-        mf1.setY(yValues);
-        fs.addMemberFunction(mf1);
+        // yValues = getY(mf1.getPoints());
+        // mf1.setY(yValues);
+        // fs.addMemberFunction(mf1);
 
-        IMemberFunction<Double> mf2 = new TrapzoidFunction<Double>(List.of(20.0, 40.0, 60.0, 80.0));
-        yValues = getY(mf2.getPoints());
-        mf2.setY(yValues);
-        fs.addMemberFunction(mf2);
+        // IMemberFunction<Double> mf2 = new TrapzoidFunction<Double>(List.of(20.0, 40.0, 60.0, 80.0));
+        // yValues = getY(mf2.getPoints());
+        // mf2.setY(yValues);
+        // fs.addMemberFunction(mf2);
 
-        IMemberFunction<Double> mf3 = new TrapzoidFunction<Double>(List.of(60.0, 80.0, 100.0, 100.0));
-        yValues = getY(mf3.getPoints());
-        mf3.setY(yValues);
-        fs.addMemberFunction(mf3);
-        Map<String , Set<pair>> rules = new HashMap<>();
-        Set<pair> tempSet = new HashSet<>();
-        for(int i = 0; i < fs.getMemberFunctions().size(); i++) {
-            IMemberFunction<Double> mf = fs.getMemberFunctions().get(i);
-            tempSet.add(new pair(FuzzysetNames.get(i), mf.getMembershipValue(25.0)));
-        }
-        rules.put(Variable, tempSet);
-        for(String key : rules.keySet()) {
-            System.out.println("Variable: " + key);
-            for(pair p : rules.get(key)) {
-                System.out.println("Fuzzy Set: " + p.getFirst() + " Membership Value: " + p.getSecond());
-            }
-        }
+        // IMemberFunction<Double> mf3 = new TrapzoidFunction<Double>(List.of(60.0, 80.0, 100.0, 100.0));
+        // yValues = getY(mf3.getPoints());
+        // mf3.setY(yValues);
+        // fs.addMemberFunction(mf3);
+        // Map<String , Set<pair>> rules = new HashMap<>();
+        // Set<pair> tempSet = new HashSet<>();
+        // for(int i = 0; i < fs.getMemberFunctions().size(); i++) {
+        //     IMemberFunction<Double> mf = fs.getMemberFunctions().get(i);
+        //     tempSet.add(new pair(FuzzysetNames.get(i), mf.getMembershipValue(25.0)));
+        // }
+        // rules.put(Variable, tempSet);
+        // for(String key : rules.keySet()) {
+        //     System.out.println("Variable: " + key);
+        //     for(pair p : rules.get(key)) {
+        //         System.out.println("Fuzzy Set: " + p.getFirst() + " Membership Value: " + p.getSecond());
+        //     }
+        // }
         
     }
     public <T extends Number> double AND(T A, T B) {
