813a72283ce9193576b8fd6887746221c8927d2b
roaa935 <122809528+roaa935@users.noreply.github.com>
2025-11-21 22:55:28 +0200

    Update Variable.java

--- FuzzyLogic/FuzzyVariables/Variable.java
+++ FuzzyLogic/FuzzyVariables/Variable.java
@@ -6,15 +6,23 @@ public class Variable{
 
     private FuzzySet fuzzySet;
     String name;
+    private double lb;
+    private double ub;
+    
     public String getName() {
         return name;
     }
-    public  Variable(String name, FuzzySet fuzzySet) {
+    public  Variable(String name, FuzzySet fuzzySet,double lb,double u) {
         this.name = name;
         this.fuzzySet = fuzzySet;
+        this.lb=lb;
+        this.ub = ub;
     }
     
     public FuzzySet getFuzzySet() {
         return fuzzySet;
     }
+
+     public getLowerBound(){return lb;}
+    public getUpperBound(){return ub;}
 }
