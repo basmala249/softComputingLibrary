183f967f5402c860ff84235691bc02cf7b462e3b
mayafouad <11410120220270@stud.cu.edu.eg>
2025-11-22 18:20:39 +0200

    add: implement GetY class to calculate yValues based on input list

---
+++ FuzzyLogic/Utils/GetY.java
@@ -0,0 +1,23 @@
+package Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetY {
+    private static final double INF = 1000000;
+    public List<Double> getY(List<Double> inputs) {
+        List<Double> yValues = new ArrayList<>();
+        for(int i = 0; i < inputs.size(); i++) {
+            if(inputs.get(i) == (double) INF) {
+                yValues.add(1.0);
+            } else{
+                if(i == 0 || i == inputs.size() - 1) {
+                    yValues.add(0.0);
+                } else {
+                    yValues.add(1.0);
+                }
+            }
+        }
+        return yValues;
+    }
+}
