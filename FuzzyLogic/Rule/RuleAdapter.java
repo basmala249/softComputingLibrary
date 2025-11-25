736643466b606913debe0412ba0b2259b5bb340e
roaa935 <122809528+roaa935@users.noreply.github.com>
2025-11-25 17:13:21 +0200

    Create RuleAdapter.java

---
+++ FuzzyLogic/Rule/RuleAdapter.java
@@ -0,0 +1,43 @@
+package Rule;
+
+import Rule.IRule;
+import Rule.MamdaniRule;
+import Rule.SugenoRule;
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+
+public class RuleAdapter implements JsonSerializer<IRule>, JsonDeserializer<IRule> {
+
+    @Override
+    public JsonElement serialize(IRule rule, Type type, JsonSerializationContext context) {
+        JsonObject obj = new JsonObject();
+        obj.addProperty("type", rule.getClass().getSimpleName());
+        obj.addProperty("condition", rule.getCondition());
+        obj.addProperty("consequence", rule.getConsequence());
+        obj.addProperty("enabled", rule.isEnabled());
+        return obj;
+    }
+
+    @Override
+    public IRule deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+
+        JsonObject obj = json.getAsJsonObject();
+        String type = obj.get("type").getAsString();
+        String condition = obj.get("condition").getAsString();
+        String consequence = obj.get("consequence").getAsString();
+
+        IRule rule;
+
+        if (type.equals("MamdaniRule"))
+            rule = new MamdaniRule(condition, consequence);
+        else
+            rule = new SugenoRule(condition, consequence);
+
+        if (!obj.get("enabled").getAsBoolean())
+            rule.setDisabled();
+
+        return rule;
+    }
+}
