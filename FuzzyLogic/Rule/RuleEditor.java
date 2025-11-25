03319078b8954500a24bf12c40ebb6f9dc42d01b
roaa935 <122809528+roaa935@users.noreply.github.com>
2025-11-25 17:12:59 +0200

    Create RuleEditor.java

---
+++ FuzzyLogic/Rule/RuleEditor.java
@@ -0,0 +1,46 @@
+package Rule;
+
+import Rule.IRule;
+import java.util.List;
+
+public class RuleEditor {
+
+    private List<IRule> rules;
+    private final RuleStorage storage;
+
+    public RuleEditor(RuleStorage storage) {
+        this.storage = storage;
+        this.rules = storage.loadRules();
+    }
+
+    public List<IRule> getAll() {
+        return rules;
+    }
+
+    public void addRule(IRule rule) {
+        rules.add(rule);
+        storage.saveRules(rules);
+    }
+
+    public void deleteRule(int index) {
+        rules.remove(index);
+        storage.saveRules(rules);
+    }
+
+    public void updateRule(int index, String newCondition, String newConsequence) {
+        IRule rule = rules.get(index);
+        rule.setCondition(newCondition);
+        rule.setConsequence(newConsequence);
+        storage.saveRules(rules);
+    }
+
+    public void enableRule(int index) {
+        rules.get(index).setEnabled();
+        storage.saveRules(rules);
+    }
+
+    public void disableRule(int index) {
+        rules.get(index).setDisabled();
+        storage.saveRules(rules);
+    }
+}
