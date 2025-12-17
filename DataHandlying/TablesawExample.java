ecf7f656010dfd58936e74d3761c97fdc541e12f
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-12-17 20:43:47 +0200

    Update TablesawExample.java

--- DataHandlying/TablesawExample.java
+++ DataHandlying/TablesawExample.java
@@ -1,9 +1,4 @@
 package DataHandlying;
-// import java.util.ArrayList;
-// import java.util.Collections;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
 
 
 
@@ -22,44 +17,15 @@ public class TablesawExample {
         NormalizeValues normalizer = new NormalizeValues(table);
         table = normalizer.normalize();
         System.out.println(table);
-        
+        OneHotEncoding encoder = new OneHotEncoding(table);
+        table = encoder.encode();
+        System.out.println(table.last(5));
         TrainTestSplit splitter = new TrainTestSplit(table);
         List<Table> split = splitter.split(0.8);
         Table train = split.get(0);
         Table test = split.get(1);
         System.out.println("Train rows: " + train.rowCount());
         System.out.println("Test rows: " + test.rowCount());
-    //     // Step 3: Encode String Columns safely
-    //     List<IntColumn> newColumns = new ArrayList<>();
-    //     Map<String, Map<String, Integer>> encoders = new HashMap<>();
-    //     Map<String, Map<Integer, String>> decoders = new HashMap<>();
 
-    //     for (Column<?> col : table.columns()) {
-    //         if (col instanceof StringColumn sc) {
-    //             IntColumn encoded = IntColumn.create(sc.name() + "_enc");
-    //             Map<String, Integer> encoder = new HashMap<>();
-    //             Map<Integer, String> decoder = new HashMap<>();
-    //             int id = 0;
-
-    //             for (String v : sc) {
-    //                 if (!encoder.containsKey(v)) {
-    //                     encoder.put(v, id);
-    //                     decoder.put(id, v);
-    //                     id++;
-    //                 }
-    //                 encoded.append(encoder.get(v));
-    //             }
-
-    //             newColumns.add(encoded); // add to temporary list
-    //             encoders.put(sc.name(), encoder);
-    //             decoders.put(sc.name(), decoder);
-    //         }
-    //     }
-    //     table.addColumns(newColumns.toArray(new IntColumn[0]));
-
-    //    
-
-    
     }
 }
-
