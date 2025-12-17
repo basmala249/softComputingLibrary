9b8889c634ef6122cf9c684514a8fe057ca8126b
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-12-17 21:18:36 +0200

    Update Main.java

--- CaseStudy/Main.java
+++ CaseStudy/Main.java
@@ -1,19 +1,21 @@
 package CaseStudy;
 
-import NeuralNetwork.NeuralLayer;
-import NeuralNetwork.NeuralNetwork;
 
-import ActivationFunction.IActivationFunction;
-import LossFunctions.ILossFunction;
-import WeightsInitialization.IWeightInitializer;
-import ActivationFunction.Sigmoid;
-import LossFunctions.MeanSquaredErrorLoss;
-import WeightsInitialization.RandInit;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.List;
+import tech.tablesaw.api.Table;
+import DataHandlying.OneHotEncoding;
+import DataHandlying.TrainTestSplit;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Table table = Table.read().csv("iris_data.csv");
+        OneHotEncoding encoder = new OneHotEncoding(table);
+        table = encoder.encode();
+        System.out.println(table.first(5));
+        TrainTestSplit splitter = new TrainTestSplit(table);
+        List<Table> split = splitter.split(0.8);
+        Table train = split.get(0);
+        Table test = split.get(1); 
 
+    }
 }
